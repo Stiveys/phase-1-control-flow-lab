f96bce7f5fcfc8ca4eae6f69c780ebb6e2173d2d
Stiveys <stevomwaura@gmail.com>
2024-09-23 19:05:09 +0300

    new

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance > 2500) {
+    return 'No can do.';
+  } else if (rideDistance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideDistance > 400) {
+    return 'That will be twenty bucks.';
+  } else {
+    return 'This one is on me!';
+  }
+}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
